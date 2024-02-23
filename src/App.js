b037249903a8ba375f6a62e1c4a0b88d7a1ee670
ahmed thousif <ahmedthousif571@gmail.com>
2024-02-24 00:15:25 +0530

    todo done

--- src/App.js
+++ src/App.js
@@ -4,9 +4,21 @@ import "./App.css";
 function App() {
   const [todo, setTodo] = useState("");
   const [todos, setTodos] = useState([]);
-
+  const [editId, setEditId] = useState(0);
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (editId) {
+      const editTodo = todos.find((i) => i.id === editId);
+      const updatedTodos = todos.map((t) =>
+        t.id === editTodo.id
+          ? (t = { id: t.id, todo })
+          : { id: t.id, todo: t.todo }
+      );
+      setTodos(updatedTodos);
+      setEditId(0);
+      setTodo("");
+      return;
+    }
 
     if (todo !== "") {
       setTodos([{ id: `${todo}-${Date.now()}`, todo }, ...todos]);
@@ -17,6 +29,12 @@ function App() {
   const handleDelete = (id) => {
     const delTodo = todos.filter((to) => to.id !== id);
     setTodos([...delTodo]);
+  };
+
+  const handleEdit = (id) => {
+    const editTodo = todos.find((i) => i.id === id);
+    setTodo(editTodo.todo);
+    setEditId(id);
   };
 
   return (
@@ -32,15 +50,15 @@ function App() {
               setTodo(e.target.value);
             }}
           />
-          <button type="submit">Go</button>
+          <button type="submit">{editId ? "Edit" : "Go"}</button>
         </form>
 
         <ul className="allTodos">
           {todos.map((t) => (
             <li className="singleTodo" key={t.id}>
               <span className="todoText">{t.todo}</span>
-              <button>Edit</button>
-              <button>Delete</button>
+              <button onClick={() => handleEdit(t.id)}>Edit</button>
+              <button onClick={() => handleDelete(t.id)}>Delete</button>
             </li>
           ))}
         </ul>
